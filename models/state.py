7385154002381eb024c4dc461b24a6302c43eaad
jamiukabir22 <kabirm474@gmail.com>
2023-08-14 03:44:20 -0700

    models

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pythin3
+"""Module State Class."""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """Represent a state.
+
+
+    Attributes:
+    name (str): The name of the state.
+    """
+
+    name = ""
